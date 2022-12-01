42c3594974d38d4b4bc5196f0696a0e88c43014c
Leonardo Lima <se.leonardolima@gmail.com>
2022-12-01 10:30:20 -0300

    Configure employees views

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def employees(request):
+    return render(request, 'employees/employees.html')
+
+def add_employee(request):
+    return redirect('employees')
+
+def edit_employee(request, id):
+    return redirect('employees')
+
+def delete_employee(request, id):
+    return redirect('employees')
