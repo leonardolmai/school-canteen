e2fa97825dc93ce5d7e0919e79568ada76b6cfde
Leonardo Lima <se.leonardolima@gmail.com>
2022-12-05 21:07:48 -0300

    Configure register sale views

--- sales/urls.py
+++ sales/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.sales, name='sales'),
+    path('add-cart-product/<int:id>', views.add_cart_product, name='add-cart-product'),
+    path('delete-cart-product/<int:id>', views.delete_cart_product, name='delete-cart-product'),
+    path('register-sale/', views.register_sale, name='register-sale'),
     path('day-report/', views.day_report, name='day-report'),
     path('week-report/', views.week_report, name='week-report'),
     path('month-report/', views.month_report, name='month-report'),
