1221fa07c23b921007fc5478544bae1dfa417b94
Leonardo Lima <se.leonardolima@gmail.com>
2022-12-07 20:45:23 -0300

    Remove cart URLs
    Remove URLs for add and delete shopping cart items as there is no need to store this temporary data

--- sales/urls.py
+++ sales/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.sales, name='sales'),
-    path('add-cart-product/<int:id>', views.add_cart_product, name='add-cart-product'),
-    path('delete-cart-product/<int:id>', views.delete_cart_product, name='delete-cart-product'),
     path('register-sale/', views.register_sale, name='register-sale'),
     path('day-report/', views.day_report, name='day-report'),
     path('week-report/', views.week_report, name='week-report'),
