ea1de9d3439ec727402fefc5a2f013fae3e68adc
Leonardo Lima <se.leonardolima@gmail.com>
2022-12-29 22:26:05 -0300

    Include django auth urls

--- schoolcanteen/urls.py
+++ schoolcanteen/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('employees/', include('employees.urls')),
     path('sales/', include('sales.urls')),
     path('products/', include('products.urls')),
