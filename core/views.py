3842f75260aad5660b88cdb91c81ccb044818003
Leonardo Lima <se.leonardolima@gmail.com>
2022-12-31 09:50:31 -0300

    Configure index view

--- core/views.py
+++ core/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+def index(request):
+    return redirect('employees')
+
 def summary(request):
     return render(request, 'core/summary.html')
