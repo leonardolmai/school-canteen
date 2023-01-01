d133c4def60deb22b7921f697d359e314fb54531
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-01 20:58:15 -0300

    Configure User Manager

--- core/models.py
+++ core/models.py
@@ -1,7 +1,39 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('O e-mail é obrigatório')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser deve ter is_staff = True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser deve ter is_superuser = True')
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     email = models.EmailField('e-mail', unique=True)
@@ -14,3 +46,5 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    objects = UserManager()
