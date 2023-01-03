0cf64fe4cd5010770329966e7d519cd2c3548c08
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-02 21:01:21 -0300

    Add classes to authentication form fields

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from .models import User
 
 class UserCreationForm(UserCreationForm):
@@ -11,3 +11,8 @@ class UserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
         fields = ('first_name', 'last_name', 'phone', 'cpf', 'photo', 'email')
+
+
+email = AuthenticationForm.base_fields['username']
+password = AuthenticationForm.base_fields['password']
+email.widget.attrs['class'] = password.widget.attrs['class'] = 'form-control login__input'
