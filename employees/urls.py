fcba2535f6fd01aadcf12e7c9c49824ebc4c4eef
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-04 13:02:05 -0300

    Change list employee view

--- employees/urls.py
+++ employees/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.employees, name='employees'),
+    path('', views.EmployeeList.as_view(), name='employees'),
     path('add/', views.add_employee, name='add_employee'),
     path('edit/<int:id>/', views.edit_employee, name='edit_employee'),
     path('delete/<int:id>/', views.delete_employee, name='delete_employee'),
--- employees/views.py
+++ employees/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.views.generic import ListView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from core.models import User
 
 # Create your views here.
 
-def employees(request):
-    return render(request, 'employees/employees.html')
+class EmployeeList(UserPassesTestMixin, ListView):
+    login_url = '/login'
+    model = User
+    template_name = 'employees/employees.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        employees = User.objects.filter(groups__name__in=['Repositor', 'Vendedor'])
+        return employees
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Gerente').exists()
+
 
 def add_employee(request):
     return redirect('employees')
