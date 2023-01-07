25a7dcecc3916e0e2a1c562e6168b9c6b015a103
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-07 14:08:05 -0300

    Customize the form to add CSS class to the fields

--- core/forms.py
+++ core/forms.py
@@ -17,6 +17,11 @@ class UserChangeForm(UserChangeForm):
         model = User
         fields = ('first_name', 'last_name', 'phone', 'cpf', 'photo', 'email')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control form__input'
+
 login_email = AuthenticationForm.base_fields['username']
 login_password = AuthenticationForm.base_fields['password']
 login_email.widget.attrs['class'] = login_password.widget.attrs['class'] = 'form-control login__input'
