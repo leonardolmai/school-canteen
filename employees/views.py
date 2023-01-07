93d047d059bbf0336ab6a5b923171d220409f9ee
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-06 22:31:42 -0300

    Change create employee view

--- employees/views.py
+++ employees/views.py
@@ -1,9 +1,49 @@
 from django.shortcuts import redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from core.models import User
+from django.contrib.auth.models import Group
+from core.forms import UserCreationForm
+from django.contrib import messages
+from django.urls import reverse_lazy
+import json
 
 # Create your views here.
+
+class EmployeeCreate(UserPassesTestMixin, CreateView):
+    login_url = '/login'
+    form_class = UserCreationForm
+    model = User
+    template_name = 'employees/add_form.html'
+    success_url = reverse_lazy('employees')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        user = form.instance
+        group_name = self.request.POST.get('group')
+
+        if group_name == 'Repositor':
+            group = Group.objects.get(name=group_name)
+            message = f'{group_name} {user.get_full_name()}, cadastrado com sucesso!'
+        else:
+            group = Group.objects.get(name='Vendedor')
+            message = f'Vendedor {user.get_full_name()}, cadastrado com sucesso!'
+
+        user.groups.add(group)
+        user.save()
+        messages.success(self.request, message)
+
+        response.headers = {
+                    'HX-Trigger': json.dumps({
+                        "showMessage": message
+                    })}
+
+        return response
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Gerente').exists()
+
 
 class EmployeeList(UserPassesTestMixin, ListView):
     login_url = '/login'
@@ -27,10 +67,7 @@ class EmployeeDetail(UserPassesTestMixin, DetailView):
 
     def test_func(self):
         return self.request.user.groups.filter(name='Gerente').exists()
-        
 
-def add_employee(request):
-    return redirect('employees')
 
 def edit_employee(request, id):
     return redirect('employees')
