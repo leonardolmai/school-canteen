f9935182b9be77e4ee2c6c209b989da5c5eb3e2d
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-07 14:06:51 -0300

    Change employee update view

--- employees/urls.py
+++ employees/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.EmployeeList.as_view(), name='employees'),
     path('add/', views.EmployeeCreate.as_view(), name='add_employee'),
     path('detail/<int:pk>', views.EmployeeDetail.as_view(), name='detail_employee'),
-    path('edit/<int:id>/', views.edit_employee, name='edit_employee'),
+    path('edit/<int:pk>/', views.EmployeeUpdate.as_view(), name='edit_employee'),
     path('delete/<int:id>/', views.delete_employee, name='delete_employee'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from core.models import User
 from django.contrib.auth.models import Group
-from core.forms import UserCreationForm
+from core.forms import UserCreationForm, UserChangeForm
 from django.contrib import messages
 from django.urls import reverse_lazy
 import json
@@ -69,8 +69,45 @@ class EmployeeDetail(UserPassesTestMixin, DetailView):
         return self.request.user.groups.filter(name='Gerente').exists()
 
 
-def edit_employee(request, id):
-    return redirect('employees')
+class EmployeeUpdate(UserPassesTestMixin, UpdateView):
+    login_url = '/login'
+    form_class = UserChangeForm
+    model = User
+    template_name = 'employees/edit_form.html'
+    success_url = reverse_lazy('employees')
+    queryset = User.objects.filter(groups__name__in=['Repositor', 'Vendedor'])
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        user = form.instance
+        group_name = self.request.POST.get('group')
+        message = f'{user.groups.all()[0]} {user.get_full_name()}, atualizado com sucesso!'
+
+        if group_name:
+            if group_name == 'Repositor':
+                group = Group.objects.get(name=group_name)
+                message = f'{group_name} {user.get_full_name()}, atualizado com sucesso!'
+            else:
+                group = Group.objects.get(name='Vendedor')
+                message = f'Vendedor {user.get_full_name()}, atualizado com sucesso!'
+
+            user.groups.clear()
+            user.groups.add(group)
+        user.save()
+        messages.success(self.request, message)
+
+        response.headers = {
+                    'HX-Trigger': json.dumps({
+                        "showMessage": message
+                    })}
+
+        return response
+
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Gerente').exists()
+
 
 def delete_employee(request, id):
     return redirect('employees')
