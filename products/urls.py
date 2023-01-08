65ad415ea2aa3a418b79313aaf7924a0daf95066
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-08 13:39:51 -0300

    Change product list view

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products, name='products'),
+    path('', views.ProductList.as_view(), name='products'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:id>/', views.edit_product, name='edit_product'),
     path('delete/<int:id>/', views.delete_product, name='delete_product'),
--- products/views.py
+++ products/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.views.generic import ListView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from .models import Product
 
 # Create your views here.
 
-def products(request):
-    return render(request, 'products/products.html')
+class ProductList(UserPassesTestMixin, ListView):
+    login_url = '/login'
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        products = Product.objects.all()
+        return products
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Gerente').exists() or self.request.user.groups.filter(name='Repositor').exists() or self.request.user.groups.filter(name='Vendedor').exists() 
+
 
 def add_product(request):
     return redirect('products')
