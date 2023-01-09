85188bdaf337dc253856deb6c41a013f2fa553ee
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-09 10:37:56 -0300

    Change client create view

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from .models import Client
+from .forms import ClientModelForm
+from django.contrib import messages
+from django.urls import reverse_lazy
+import json
 
 # Create your views here.
 
@@ -28,8 +32,29 @@ class ClientDetail(UserPassesTestMixin, DetailView):
         return self.request.user.groups.filter(name='Gerente').exists() or self.request.user.groups.filter(name='Vendedor').exists() 
 
 
-def add_client(request):
-    return redirect('clients')
+class ClientCreate(UserPassesTestMixin, CreateView):
+    login_url = '/login'
+    form_class = ClientModelForm
+    model = Client
+    template_name = 'clients/add_form.html'
+    success_url = reverse_lazy('clients')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        client = form.instance
+        message = f'Cliente {client.first_name}, cadastrado com sucesso!'
+        messages.success(self.request, message)
+
+        response.headers = {
+                    'HX-Trigger': json.dumps({
+                        "showMessage": message
+                    })}
+
+        return response
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Gerente').exists() or self.request.user.groups.filter(name='Vendedor').exists() 
+
 
 def edit_client(request, id):
     return redirect('clients')
