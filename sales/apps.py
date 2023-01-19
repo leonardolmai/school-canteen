ee26f41a7b3de3bbedd157a03b8cca864344ffdb
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-19 11:54:20 -0300

    Connect signal product_sale_post_save

--- sales/apps.py
+++ sales/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
-
+from django.db.models import signals
+from .signals import product_sale_post_save
 
 class SalesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sales'
+
+    def ready(self): 
+        from .models import Sale
+
+        signals.post_save.connect(product_sale_post_save, sender=Sale.products.through)
