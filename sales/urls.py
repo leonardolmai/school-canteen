a8f86b57d3fbda027d7f5c9c931f011d0b3011d3
Leonardo Lima <se.leonardolima@gmail.com>
2023-01-19 20:23:27 -0300

    Change sale list view

--- sales/urls.py
+++ sales/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sales, name='sales'),
+    path('', views.SaleList.as_view(), name='sales'),
     path('register-sale/', views.register_sale, name='register-sale'),
     path('day-report/', views.day_report, name='day-report'),
     path('week-report/', views.week_report, name='week-report'),
--- sales/views.py
+++ sales/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from .models import Sale
 
 # Create your views here.
 
-def sales(request):
-    return render(request, 'sales/sales.html')
+class SaleList(UserPassesTestMixin, ListView):
+    login_url = '/login'
+    model = Sale
+    template_name = 'sales/sales.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        sales = Sale.objects.all()
+        return sales
+    
+    def test_func(self):
+        return self.request.user.groups.filter(name='Gerente').exists() or self.request.user.groups.filter(name='Vendedor').exists()
 
 def register_sale(request):
     if request.method == 'GET':
