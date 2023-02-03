466c0f9501ec97316b5c1491c16278233535290d
Leonardo Lima <se.leonardolima@gmail.com>
2023-02-03 20:55:40 -0300

    Add current product price at time of sale after saving product_sale

--- sales/signals.py
+++ sales/signals.py
@@ -5,3 +5,7 @@ def product_sale_post_save(sender, instance, *args, **kwargs):
         total += product.product.price * product.quantity
     instance.sale.total = total
     instance.sale.save()
+    instance.price = instance.product.price
+    instance._meta.auto_created = True
+    instance.save()
+    instance._meta.auto_created = False
