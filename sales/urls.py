5465097a6f2242eadd4801103a72f4845b02d62b
Leonardo Lima <se.leonardolima@gmail.com>
2023-02-04 17:17:35 -0300

    Change the view to generate the day's sales report

--- sales/urls.py
+++ sales/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.SaleList.as_view(), name='sales'),
     path('register-sale/', views.RegisterSale.as_view(), name='register-sale'),
     path('detail/<int:pk>', views.SaleDetail.as_view(), name='detail_sale'),
-    path('day-report/', views.day_report, name='day-report'),
+    path('day-report/', views.DayReport.as_view(), name='day-report'),
     path('week-report/', views.week_report, name='week-report'),
     path('month-report/', views.month_report, name='month-report'),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,11 +1,17 @@
+import io
+import datetime
 from django.shortcuts import redirect
+from django.views import View
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from .models import Sale, Product_Sale
 from products.models import Product
+from django.db.models.aggregates import Count, Sum
 from clients.models import Client
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
 
 # Create your views here.
 
@@ -79,8 +85,62 @@ class RegisterSale(UserPassesTestMixin, CreateView):
         return self.request.user.groups.filter(name='Gerente').exists() or self.request.user.groups.filter(name='Vendedor').exists()
 
 
-def day_report(request):
-    return redirect('sales')
+class DayReport(UserPassesTestMixin, View):
+    login_url = '/login'
+   
+    def get(self, request):
+        buffer = io.BytesIO()
+        pdf = canvas.Canvas(buffer)
+
+        current_date = datetime.date.today()
+
+        pdf.setTitle(f'day-report-{current_date}')
+        pdf.setFont('Helvetica-Oblique', 14)
+        pdf.drawString(220, 820, f'Relatório do dia {current_date.strftime("%d/%m/%Y")}')
+       
+        payment_method = Sale.objects.filter(datetime__day=current_date.day).values('payment_method').annotate(count=Count('payment_method')).order_by('-count').first()
+        pdf.setFont('Helvetica', 12)
+        if payment_method != None:
+            pdf.drawString(10, 790, f'Método de pagamento mais utilizado: {payment_method["payment_method"]}, em {payment_method["count"]} vendas.')
+
+        sold_amount = sum([product.quantity for product in Product_Sale.objects.filter(sale__datetime__day=current_date.day)])
+        pdf.drawString(10, 770, f'Foram vendidos: {sold_amount} produtos.')
+
+        total_sales_value = Sale.objects.filter(datetime__day=current_date.day).aggregate(Count('id'), Sum('total'))
+        pdf.drawString(10, 750, f'Valor total de vendas: R${total_sales_value["total__sum"]:.2f}')
+
+        y = 720
+        pdf.setFont('Helvetica-Bold', 10)
+        pdf.drawString(10, y, 'ID')
+        pdf.drawString(60, y, 'CPF cliente')
+        pdf.drawString(160, y, 'Método de pagamento')
+        pdf.drawString(280, y, 'Total')
+        pdf.drawString(380, y, 'Data e hora')
+        pdf.line(10, y-5, 585, y-5)
+        y = y - 20
+
+        for sale in Sale.objects.filter(datetime__day=current_date.day):
+            if y <= 40:
+                pdf.showPage()
+                y = 790
+            pdf.setFont('Helvetica', 10)
+            pdf.drawString(10, y, f'{sale}')
+            pdf.drawString(60, y, f'{sale.client.cpf}')
+            pdf.drawString(160, y, f'{sale.payment_method}')
+            pdf.drawString(280, y, f'R${sale.total:.2f}')
+            pdf.drawString(380, y, f'{sale.datetime.strftime("%d/%m/%Y às %H:%M")}')
+            pdf.line(10, y-5, 585, y-5)
+            y = y - 20
+
+        pdf.showPage()
+        pdf.save()
+
+        buffer.seek(0)
+
+        return FileResponse(buffer, as_attachment=True, filename=f'day-report-{current_date}.pdf')
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Gerente').exists()
 
 def week_report(request):
     return redirect('sales')
